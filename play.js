6033940665a06114f300b5126fca3a3882e0d732
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-02-22 22:52:17 +0000

    Play starter code

--- play.js
+++ play.js
@@ -1,22 +1,6 @@
 const net = require("net");
-
+const {connect} = require('./client.js');
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '10.0.2.15',
-    port: 50541
-  });
-conn.on('data', (data) => {
-  console.log('data:', data)
-})
-conn.on('timeout', () => {
-  console.log('you ded cuz you idled');
-})
- 
-  conn.setEncoding("utf8");
-
-  return conn;
-};
 
 
 
