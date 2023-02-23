e7bbf46b90e20a1f95cca01960cfb585e101c7cd
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-02-23 20:27:20 +0000

    Play file condensed

--- play.js
+++ play.js
@@ -1,21 +1,6 @@
 const net = require("net");
 const {connect} = require('./client.js');
-// establishes a connection with the game server
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
+const { setupInput } = require('./input');
 
 
 console.log("Connecting ...");
