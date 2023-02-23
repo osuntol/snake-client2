1a8f5bb23d9ac83632f58bc68f4be3643eddb875
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-02-23 20:16:46 +0000

    Play with stdin

--- play.js
+++ play.js
@@ -2,7 +2,22 @@ const net = require("net");
 const {connect} = require('./client.js');
 // establishes a connection with the game server
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
 
 
 console.log("Connecting ...");
 connect();
+setupInput();
